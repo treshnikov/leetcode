e4430370c157ca29b63f8684f062a3980a98e579
pavel.treshnikov <treshnikov@gmail.com>
2020-11-15 18:27:49 +0400

    Add solution for the Container With Most Water problem

---
+++ src/Problems/ContainerWithMostWater.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public static class ContainerWithMostWater
+    {
+        // https://leetcode.com/problems/container-with-most-water/
+        public static int MaxArea(int[] height)
+        {
+            var left = 0;
+            var right = height.Length - 1;
+            var maxSqueare = 0;
+            var square = 0;
+
+            while (left < right)
+            {
+                if (height[left] < height[right])
+                {
+                    square = (right - left) * height[left];
+                    if (square > maxSqueare)
+                        maxSqueare = square;
+                    left = left + 1;
+                }
+                else
+                {
+                    square = (right - left) * height[right];
+                    if (square > maxSqueare)
+                    {
+                        maxSqueare = square;
+                    }
+                    right = right - 1;
+                }
+            }
+
+            return maxSqueare;
+        }
+
+    }
+}
